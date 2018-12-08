9b60a23f9a7995ad55753a74d4d4ac0ff6b7ab5b
Jorge Shirai <jlshirai@gmail.com>
2018-12-08 19:05:00 -0300

    Deploy UserClaimHolder & ClaimVerifier on dev.
    Deploy the corresponding contracts to test the erc735/erc725 feature in 
    develoment network (truffle).

--- blockchain/migrations/1525290845_deploy_poba.js
+++ blockchain/migrations/1525290845_deploy_poba.js
@@ -1,5 +1,13 @@
 const PoBA = artifacts.require('./PoBA.sol')
+const UserClaimHolder = artifacts.require('./UserClaimHolder.sol')
+const ClaimVerifier = artifacts.require('./ClaimVerifier.sol')
 
-module.exports = function(deployer) {
+module.exports = function(deployer, network, accounts) {
   deployer.deploy(PoBA)
+
+  // Use the 5th account to interact with the DApp
+  if (network === 'development') {
+    deployer.deploy(UserClaimHolder, { from: accounts[4] })
+    deployer.deploy(ClaimVerifier)
+  }
 }
